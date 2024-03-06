34e1e110b37dec4db55bf4362140666422767e3d
ernest <efran7107@gmail.com>
2024-03-05 18:51:29 -0700

    switch tabs functional
    created way to switch tabs in function

--- src/Functional/FunctionalSection.tsx
+++ src/Functional/FunctionalSection.tsx
@@ -1,31 +1,55 @@
 // you can use this type for react children if you so choose
-import { ReactNode } from "react";
-import { Link } from "react-router-dom";
+import { ReactNode } from 'react';
+import { Link } from 'react-router-dom';
+import { Dog } from '../types';
 
-export const FunctionalSection = ({ children }: { children: ReactNode }) => {
-  return (
-    <section id="main-section">
-      <div className="container-header">
-        <div className="container-label">Dogs: </div>
-        <Link to={"/class"} className="btn">
-          Change to Class
-        </Link>
-        <div className="selectors">
-          {/* This should display the favorited count */}
-          <div className={`selector active`} onClick={() => {}}>
-            favorited ( {} )
-          </div>
+export const FunctionalSection = ({
+	children,
+	fav,
+	handleFav,
+	dogSort,
+}: {
+	children: ReactNode;
+	fav: boolean | undefined;
+	handleFav: (fav: boolean) => void;
+	dogSort: [Dog[], Dog[]];
+}) => {
+	const [favDogs, unFavDogs] = dogSort;
+	return (
+		<section id='main-section'>
+			<div className='container-header'>
+				<div className='container-label'>Dogs: </div>
+				<Link
+					to={'/class'}
+					className='btn'>
+					Change to Class
+				</Link>
+				<div className='selectors'>
+					{/* This should display the favorited count */}
+					<div
+						className={`selector ${fav === true ? `active` : null}`}
+						onClick={() => {
+							handleFav(true);
+						}}>
+						favorited ( {favDogs.length} )
+					</div>
 
-          {/* This should display the unfavorited count */}
-          <div className={`selector`} onClick={() => {}}>
-            unfavorited ( {} )
-          </div>
-          <div className={`selector`} onClick={() => {}}>
-            create dog
-          </div>
-        </div>
-      </div>
-      <div className="content-container">{children}</div>
-    </section>
-  );
+					{/* This should display the unfavorited count */}
+					<div
+						className={`selector ${fav === false ? `active` : null}`}
+						onClick={() => {
+							handleFav(false);
+						}}>
+						unfavorited ( {unFavDogs.length} )
+					</div>
+					<div
+						className={`selector`}
+						onClick={() => {}}>
+						create dog
+					</div>
+				</div>
+			</div>
+			<div className='content-container'>{children}</div>
+		</section>
+	);
 };
