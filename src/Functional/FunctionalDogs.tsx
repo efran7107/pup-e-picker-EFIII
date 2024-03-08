9d620f5f33f564473638990a4dda31f800a58e19
efran7107 <efran7107@gmail.com>
2024-03-07 18:08:21 -0700

    figuring it out

--- src/Functional/FunctionalDogs.tsx
+++ src/Functional/FunctionalDogs.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { DogCard } from "../Shared/DogCard";
 import { Dog } from "../types";
 
 // Right now these dogs are constant, but in reality we should be getting these from our server
 export const FunctionalDogs = ({ allDogs }: { allDogs: Dog[] }) => {
+  const [dogs, setDogs] = useState<Dog[]>(allDogs);
+
   return (
     //  the "<> </>"" are called react fragments, it's like adding all the html inside
     // without adding an actual html element
--- src/api.tsx
+++ src/api.tsx
@@ -1,22 +1,30 @@
-import { Dog } from './types';
+import { Dog } from "./types";
 
-export const baseUrl = 'http://localhost:3000';
+export const baseUrl = "http://localhost:3000";
 
 export const Requests = {
-	// should return a promise with all dogs in the database
-	getAllDogs: (): Promise<Dog[]> =>
-		fetch(`${baseUrl}/dogs`).then((dogs) => dogs.json()),
-	// should create a dog in the database from a partial dog object
-	// and return a promise with the result
-	postDog: () => {},
+  // should return a promise with all dogs in the database
+  getAllDogs: (): Promise<Dog[]> =>
+    fetch(`${baseUrl}/dogs`).then((dogs) => dogs.json()),
+  // should create a dog in the database from a partial dog object
+  // and return a promise with the result
+  postDog: () => {},
 
-	// should delete a dog from the database
-	deleteDog: () => {},
+  // should delete a dog from the database
+  deleteDog: () => {},
 
-	updateDog: () => {},
+  updateDog: (dog: Dog) => {
+    fetch(`${baseUrl}/${dog.id}`, {
+      body: JSON.stringify(dog),
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+    }).then((updatedDog) => updatedDog.json);
+  },
 
-	// Just a dummy function for use in the playground
-	dummyFunction: () => {
-		console.log('dummy stuff');
-	},
+  // Just a dummy function for use in the playground
+  dummyFunction: () => {
+    console.log("dummy stuff");
+  },
 };
